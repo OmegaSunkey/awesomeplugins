f220a4f6d47f031c02d1b6e33b4ee10fd13aa958
OmegaSunkey <94485936+OmegaSunkey@users.noreply.github.com>
2022-03-18 17:14:27 -0500

    the
    filled info

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,9 +40,9 @@ subprojects {
 
     // Fill out with your info
     aliucord {
-        author("DISCORD USERNAME", 123456789L)
-        updateUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/updater.json")
-        buildUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/%s.zip")
+        author("OmegaSunkey", 9366)
+        updateUrl.set("https://raw.githubusercontent.com/OmegaSunkey/awesomeplugins/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/OmegaSunkey/awesomeplugins/builds/%s.zip")
     }
 
     android {
