4f5030fca3d9bb37499f7b899ec86c53b0854817
OmegaSunkey <bronytecfan@gmail.com>
2022-03-19 14:35:39 -0500

    added file

---
+++ startup_sound/src/main/kotlin/com/github/yournamehere/Startup_Sound.kt
@@ -0,0 +1,46 @@
+package com.github.yournamehere
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.utils.RxUtils.onBackpressureBuffer
+import com.aliucord.utils.RxUtils.subscribe
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.*
+
+// Aliucord Plugin annotation. Must be present on the main class of your plugin
+@AliucordPlugin(requiresRestart = false /* Whether your plugin requires a restart after being installed/updated */)
+// Plugin class. Must extend Plugin and override start and stop
+// Learn more: https://github.com/Aliucord/documentation/blob/main/plugin-dev/1_introduction.md#basic-plugin-structure
+class MyFirstPatch : Plugin() {
+    override fun start(context: Context) {
+        startupdiscord()
+    }
+
+private fun startupdiscord() {
+        try {
+            Utils.threadPool.execute {
+                MediaPlayer().apply {
+                    setAudioAttributes(
+                        AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+                    )
+                    setDataSource("https://github.com/OmegaSunkey/awesomeplugins/blob/main/Discord%20Startup%20Sound%20HQ.mp3?raw=true")
+                    prepare()
+                    start()
+                }
+            }
+        } catch (ignored: Throwable) {
+            // ill leave this empty
+        }
+    }
+
+    override fun stop(context: Context) {
+        // Remove all patches
+        patcher.unpatchAll()
+    }
+} // literally c+p from animal's repo lmao how do i build on github help
