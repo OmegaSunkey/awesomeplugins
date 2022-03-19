f217e7ea2c9407ad3235a139611aaa1ae2f56d24
OmegaSunkey <bronytecfan@gmail.com>
2022-03-19 14:17:39 -0500

    commit for plugins yay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,3 @@ include(
     "Startup_Sound"
 )
 
-// This is required because plugins are in the ExamplePlugins/kotlin subdirectory.
-//
-// Assuming you put all your plugins into the project root, so on the same
-// level as this file, simply remove everything below.
-//
-// Otherwise, if you want a different structure, for example all plugins in a folder called "plugins",
-// then simply change the path
-rootProject.children.forEach {
-    // Change kotlin to java if you'd rather use java
-    it.projectDir = file("ExamplePlugins/kotlin/${it.name}")
-}
