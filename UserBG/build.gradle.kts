83ddb360fcba7e58acc95eb917fffa5c964dd1b9
OmegaSunkey <bronytecfan@gmail.com>
2022-03-21 12:22:35 -0500

    build gradle kts

--- UserBG/build.gradle.kts
+++ UserBG/build.gradle.kts
@@ -1,10 +1,10 @@
-version = "1.2.3"
+version = "1.0.0"
 description = "UserBG"
 
 aliucord {
     changelog.set(
         """
-        * Update to new URL for USRBG
+        * most of this plugin was made by halalking, i just fixed some things.
     """.trimIndent()
     )
 }
