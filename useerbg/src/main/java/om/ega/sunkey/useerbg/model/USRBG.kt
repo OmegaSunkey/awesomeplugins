e8f63a6b324392ac99290eaa19b9bea32f250d39
OmegaSunkey <bronytecfan@gmail.com>
2022-03-24 16:09:57 -0500

    idk trying matcher find

--- useerbg/src/main/java/om/ega/sunkey/useerbg/model/USRBG.kt
+++ useerbg/src/main/java/om/ega/sunkey/useerbg/model/USRBG.kt
@@ -48,9 +48,9 @@ object USRBG : AbstractDatabase() {
 				id.toString() + regex,
 				Pattern.DOTALL
 			).matcher(data)
-			matcher.find()
-			usrbg.log.debug(matcher.find().toString() + " matcherfind")
-                    if (matcher.find() == false) {
+			matcher.find(1)
+			usrbg.log.debug(matcher.find(1).toString() + " matcherfind")
+                    if (matcher.find()) {
                         matcher.group(1)?.let { it1 ->
                             mapCache[id] = it1
                             it.result = it1
