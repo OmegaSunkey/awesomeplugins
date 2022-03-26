6bb22d9e9a74e039ce61542927311aec9539ddb0
OmegaSunkey <bronytecfan@gmail.com>
2022-03-26 18:22:24 -0500

    added custom sound to StartupSound

--- StartupSound/build.gradle.kts
+++ StartupSound/build.gradle.kts
@@ -1,13 +1,14 @@
-version = "1.0.0" // Plugin version. Increment this to trigger the updater
+version = "1.1.0" // Plugin version. Increment this to trigger the updater
 description = "Startup sound for discord!" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set("""
-        nothing to see here now
+        * 1.1.0
+	* Added customizable sound for startup, please provide a link to that sound (you can use cdn.discordapp.com)
     """.trimIndent())
     // Image or Gif that will be shown at the top of your changelog page
-    // changelogMedia.set("https://cool.png")
+    changelogMedia.set("https://cdn.discordapp.com/attachments/929565544334647356/957419019500146708/Screenshot_20220326-182112113.jpg")
 
     // Add additional authors to this plugin
     // author("Name", 0)
