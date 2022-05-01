98f2c27bb76883eb80ecb66d5f9fb359b0a821da
OmegaSunkey <bronytecfan@gmail.com>
2022-05-01 12:15:00 -0500

    im gonna test now

--- FanCopypasta/src/main/kotlin/om/ega/sunkey/FanCopypasta/FanCopypasta.kt
+++ FanCopypasta/src/main/kotlin/om/ega/sunkey/FanCopypasta/FanCopypasta.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import com.aliucord.Utils
 import com.aliucord.api.CommandsAPI
 import com.aliucord.api.CommandsAPI.CommandResult
-
 import com.aliucord.entities.CommandContext
+
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.*
+
 import com.discord.api.commands.ApplicationCommandType
 
 @AliucordPlugin(requiresRestart = false)
 class FanCopypasta : Plugin() {
     override fun start(context: Context) {
         commands.registerCommand("FanCopypasta", "Funny copypasta with your own word", commandoptions) {
-		val keyw = it.getRequiredSubCommandArgs("word")?.toString()
-		val copypasta = "if `${keyw}` has a million fans im one of them \nif `${keyw}` has 0 fans then I am no more \nif `${keyw}` has 1 fan that fan is me \nif the world is against `${keyw}` then I'm against the world"
-		return@registerCommand CommandResult(copypasta)
+		val keyw = it.getRequiredSubCommandArgs("word")
+		// val copypasta = "if `${keyw}` has a million fans im one of them \nif `${keyw}` has 0 fans then I am no more \nif `${keyw}` has 1 fan that fan is me \nif the world is against `${keyw}` then I'm against the world"
+		return@registerCommand CommandResult(keyw) //testing nlw lol 
 	}
    }
 
