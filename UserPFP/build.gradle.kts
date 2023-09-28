baaffaaa25731cf1e3ecc16bcbd0a4572d3fe454
OmegaSunkey <bronytecfan@gmail.com>
2023-09-28 18:31:37 -0500

    Changelog change

--- UserPFP/build.gradle.kts
+++ UserPFP/build.gradle.kts
@@ -1,18 +1,21 @@
-version = "1.0.4"
+version = "1.1.0"
 description = "UserPFP, for animated profile pictures"
 
 aliucord {
     changelog.set(
         """
         - 1.0.0
-		release; fix HalalKing source and add new db source
+		release; fix HalalKing source and add new db source.
 		- 1.0.1
-		changed regex to ignore ?=
+		changed regex to ignore ?=.
 		- 1.0.2
-		Added debug setting for knowing when a pfp is incorrect
+		Added debug setting for knowing when a pfp is incorrect.
 		- 1.0.3
-		(Added fallback pfp to be used when an error occurs while trying to load pfp) disabled for now
-		Made debug logs setting be false by default
+		(Added fallback pfp to be used when an error occurs while trying to load pfp.) disabled for now.
+		Made debug logs setting be false by default.
+		- 1.1.0
+		Switch to USRPFP database, which is used on Vencord and Vendetta.
+		Unhook while on ChatView, to evade lag and crashes.
     """.trimIndent()
     )
     author("HalalKing", 0)
