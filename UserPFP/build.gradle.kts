c4c2e0dd9e44ecf8ea1d1f6d9ac244d9a46316b5
OmegaSunkey <bronytecfan@gmail.com>
2023-09-28 16:59:03 -0500

    Changelog changed

--- UserPFP/build.gradle.kts
+++ UserPFP/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0.2"
+version = "1.0.3"
 description = "UserPFP, for animated profile pictures"
 
 aliucord {
@@ -10,6 +10,9 @@ aliucord {
 		changed regex to ignore ?=
 		- 1.0.2
 		Added debug setting for knowing when a pfp is incorrect
+		- 1.0.3
+		Added fallback pfp to be used when an error occurs while trying to load pfp
+		Made debug logs setting be false by default
     """.trimIndent()
     )
     author("HalalKing", 0)
