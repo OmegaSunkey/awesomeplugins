77103eda643a2b7e4688de3ecf9dec7340df5097
OmegaSunkey <bronytecfan@gmail.com>
2025-10-21 10:39:05 -0500

    Switch to Home on any tab; switch to last channel on home press

--- DMTabs/build.gradle.kts
+++ DMTabs/build.gradle.kts
@@ -1,10 +1,12 @@
-version = "1.0.1" // Plugin version. Increment this to trigger the updater
+version = "1.0.2" // Plugin version. Increment this to trigger the updater
 description = "Switches the DM button with the Friends button and viceversa" // Plugin description that will be shown to user
 
 aliucord {
     changelog.set("""
         # 1.0.1
 	DM button now switches you to Home Tab.
+	# 1.0.2
+	The home button now switches you to the last selected channel! Also fixes not showing DMs if youre on any other tab.
     """.trimIndent())
 
     // Excludes this plugin from the updater, meaning it won't show up for users.
--- DMTabs/src/main/kotlin/om/ega/sunkey/dmtabs/dmtabs.kt
+++ DMTabs/src/main/kotlin/om/ega/sunkey/dmtabs/dmtabs.kt
@@ -7,9 +7,12 @@ import androidx.fragment.app.FragmentManager
 import com.discord.widgets.guilds.list.GuildListItem
 import com.discord.widgets.guilds.list.WidgetGuildsListViewModel
 import com.discord.widgets.tabs.`TabsHostBottomNavigationView$updateView$5`
+import com.discord.widgets.tabs.`TabsHostBottomNavigationView$updateView$4`
 import com.discord.widgets.tabs.NavigationTab
 import com.discord.stores.StoreStream
+import com.discord.stores.StoreChannelsSelected
 import com.discord.stores.StoreTabsNavigation
+import com.discord.api.channel.Channel
 
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
@@ -20,9 +23,13 @@ import com.aliucord.patcher.*
 @AliucordPlugin
 class DMTabs : Plugin() {
     val Logger = Logger("DMTabs")
+    var lastchannel: Long = 0L
+    var shouldchangechannels = false
     override fun start(c: Context) {
 	patcher.before<`TabsHostBottomNavigationView$updateView$5`>("onClick", View::class.java) {
-		if(StoreStream.Companion!!.getTabsNavigation().getSelectedTab() == NavigationTab.FRIENDS) StoreStream.Companion!!.getTabsNavigation().selectTab(NavigationTab.HOME, false)
+		lastchannel = StoreChannelsSelected.`access$getSelectedChannel$p`(StoreStream.getChannelsSelected()).getId()
+		shouldchangechannels = true
+		if(StoreStream.Companion!!.getTabsNavigation().getSelectedTab() != NavigationTab.HOME) StoreStream.Companion!!.getTabsNavigation().selectTab(NavigationTab.HOME, false)
 		StoreStream.getGuildSelected().set(0L)
 		it.result = null	
 	}	
@@ -38,7 +45,13 @@ class DMTabs : Plugin() {
 				it.result = null
 			} else return@PreHook
 		}
-	)	
+	)
+	patcher.before<`TabsHostBottomNavigationView$updateView$4`>("onClick", View::class.java) {
+		if(shouldchangechannels) {
+			StoreStream.getMessagesLoader().jumpToMessage(lastchannel, 1L)
+			shouldchangechannels = false
+		}
+	}
     }
     override fun stop(c: Context) {
 	patcher.unpatchAll() 
