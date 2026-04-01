3ab7094a458e9c9f7c2cbc14612269a933adeaa0
OmegaSunkey <omegasunkey@tutamail.com>
2026-04-01 09:11:39 -0500

    Test new ad system

--- plugins/Ads/src/main/kotlin/om/ega/sunkey/Ads/Ads.kt
+++ plugins/Ads/src/main/kotlin/om/ega/sunkey/Ads/Ads.kt
@@ -9,14 +9,20 @@ import com.aliucord.api.CommandsAPI
 import com.aliucord.api.CommandsAPI.CommandResult
 import com.aliucord.entities.CommandContext
 import com.aliucord.Http
-
+import com.aliucord.entities.MessageEmbedBuilder
+import com.aliucord.wrappers.embeds.MessageEmbedWrapper.Companion.title
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.*
 
 import java.net.URLEncoder
+import java.util.regex.Pattern
+import java.util.regex.Matcher
 
 import com.discord.api.commands.ApplicationCommandType
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.widgets.chat.list.entries.ChatListEntry
+import com.discord.widgets.chat.list.entries.MessageEntry
 
 @AliucordPlugin(requiresRestart = false)
 class Ads : Plugin() {
@@ -31,9 +37,39 @@ class Ads : Plugin() {
 		val unixepoch = System.currentTimeMillis()
 		val uid = MemInfo.availMem + unixepoch
 		settings.setString("uid", uid.toString())
-		settings.setBool("registered", true)
-		LOG.debug(uid.toString())
-		//Http.simpleGet("https://aliucord-ads.gdspikes.workers.dev/register?uid=${uid}")
+		//LOG.debug(uid.toString())
+		try {
+			Http.simpleGet("https://aliucord-ads.gdspikes.workers.dev/register?uid=${uid}")
+			settings.setBool("registered", true)
+		} catch {
+			LOG.error("Couldn't register !! WTF!?")
+		}
+	}
+	
+	val adlist = mutableListOf<String>()
+	val AdsJSON = Http.simpleGet("https://aliucord-ads.gdspikes.workers.dev/getAd")
+	val admatch = Pattern.compile("ad\":\"\\(.*?\\)\"").matcher(AdsJSON)
+	while(admatch.find()) {
+		adlist.add(admatch.group())
+	}
+
+	patcher.after<WidgetChatListAdapterItemMessage>(
+		"onConfigure",
+		Int::class.java,
+		ChatListEntry::class.java
+	) { p ->
+		val entry = param.args[1] as MessageEntry
+		val message = entry.message
+		if (message.isLoading) return@after
+		message.embeds.removeAll {
+			it.title == "AD"
+		}
+
+		MessageEmbedBuilder().run {
+			setTitle("AD")
+			addField("Ad served from our customers", adlist.random(), false)
+			message.embeds.add(build())
+		}
 	}
 
         commands.registerCommand("newad", "Insert a new ad for all Aliucord users to see", commandoptions) {
